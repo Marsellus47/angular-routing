b4a7811729e081893a08dd37c4b12ac3929498e2
Marsellus47 <mjackow@gmail.com>
2017-09-17 14:22:39 +0200

    Module 5 - Route Parameters

--- src/app/products/product-routing.module.ts
+++ src/app/products/product-routing.module.ts
@@ -2,9 +2,13 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit.component';
 
 const ROUTES = [
-  { path: 'products', component: ProductListComponent }
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'products/:id/edit', component: ProductEditComponent }
 ];
 
 @NgModule({
