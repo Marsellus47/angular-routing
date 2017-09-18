4b9fcb4f7ddf2b58e1f1e10338e9246184473de0
Marsellus47 <mjackow@gmail.com>
2017-09-18 20:21:12 +0200

    Module 7 - Child Routes

--- src/app/products/product-routing.module.ts
+++ src/app/products/product-routing.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit.component';
+import { ProductEditInfoComponent } from './product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit-tags.component';
+
 import { ProductResolver } from './product-resolver.service';
 
 const ROUTES = [
@@ -16,7 +19,22 @@ const ROUTES = [
   {
     path: 'products/:id/edit',
     component: ProductEditComponent,
-    resolve: { product: ProductResolver }
+    resolve: { product: ProductResolver },
+    children: [
+      {
+        path: '',
+        redirectTo: 'info',
+        pathMatch: 'full'
+      },
+      {
+        path: 'info',
+        component: ProductEditInfoComponent
+      },
+      {
+        path: 'tags',
+        component: ProductEditTagsComponent
+      }
+    ]
   }
 ];
 
--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit.component';
+import { ProductEditInfoComponent } from './product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit-tags.component';
 
 import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductService } from './product.service';
@@ -19,7 +21,9 @@ import { SharedModule } from '../shared/shared.module';
     ProductListComponent,
     ProductDetailComponent,
     ProductEditComponent,
-    ProductFilterPipe
+    ProductFilterPipe,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ],
   providers: [
     ProductService
