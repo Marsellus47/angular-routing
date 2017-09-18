b825e6258523eaca218d3b3beaa7e0a2159efa0b
Marsellus47 <mjackow@gmail.com>
2017-09-18 19:25:55 +0200

    Module 6 - Prefetching Data Using Route Resolvers

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,7 +2,6 @@ import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { IProduct } from './product';
-import { ProductService } from './product.service';
 
 @Component({
     templateUrl: './app/products/product-detail.component.html'
@@ -12,17 +11,9 @@ export class ProductDetailComponent implements OnInit {
     product: IProduct;
     errorMessage: string;
 
-    constructor(private productService: ProductService,
-        private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute) { }
 
     ngOnInit(): void {
-        const id = +this.route.snapshot.paramMap.get('id');
-        this.getProduct(id);
-    }
-
-    getProduct(id: number) {
-        this.productService.getProduct(id).subscribe(
-            product => this.product = product,
-            error => this.errorMessage = <any>error);
+        this.product = this.route.snapshot.data['product'];
     }
 }
--- src/app/products/product-routing.module.ts
+++ src/app/products/product-routing.module.ts
@@ -4,11 +4,20 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit.component';
+import { ProductResolver } from './product-resolver.service';
 
 const ROUTES = [
   { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'products/:id/edit', component: ProductEditComponent }
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
+    resolve: { product: ProductResolver }
+  },
+  {
+    path: 'products/:id/edit',
+    component: ProductEditComponent,
+    resolve: { product: ProductResolver }
+  }
 ];
 
 @NgModule({
@@ -17,6 +26,9 @@ const ROUTES = [
   ],
   exports: [
     RouterModule
+  ],
+  providers: [
+    ProductResolver
   ]
 })
 export class ProductRoutingModule { }
