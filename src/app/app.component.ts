48e89c69666773edbadb4fd765bf6964a2daccfa
Marsellus47 <mjackow@gmail.com>
2017-09-20 18:32:00 +0200

    Module 9 - Styling, Animating, and Watching Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Router } from '@angular/router';
+import {
+    Router,
+    Event,
+    NavigationStart,
+    NavigationEnd,
+    NavigationError,
+    NavigationCancel
+} from '@angular/router';
 import { Component } from '@angular/core';
 
 import { AuthService } from './user/auth.service';
@@ -9,9 +16,26 @@ import { AuthService } from './user/auth.service';
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
+    loading: boolean = true;
 
     constructor(private authService: AuthService,
-        private router: Router) { }
+        private router: Router) {
+        this.router.events.subscribe(routerEvent => {
+            this.checkRouterEvent(routerEvent);
+        });
+    }
+
+    checkRouterEvent(routerEvent: Event): void {
+        if (routerEvent instanceof NavigationStart) {
+            this.loading = true;
+        }
+
+        if (routerEvent instanceof NavigationEnd ||
+            routerEvent instanceof NavigationCancel ||
+            routerEvent instanceof NavigationError) {
+            this.loading = false;
+        }
+    }
 
     logOut(): void {
         this.authService.logout();
